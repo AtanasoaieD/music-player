ef36e47e9e71ab96baa51f13948056df58fd0e89
AtanasoaieD <danatanasoaie17@gmail.com>
2024-08-25 18:32:08 +0300

    progress bar

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img')
 const title = document.getElementById('title')
 const artist = document.getElementById('artist')
 const music = document.querySelector('audio')
+const progressContainer = document.getElementById('progress-container')
+const progress = document.getElementById('progress')
 const prevBtn = document.getElementById('prev')
 const playBtn = document.getElementById('play')
 const nextBtn = document.getElementById('next')
@@ -76,7 +78,7 @@ let songIndex=0;
 function prevSong(){
     songIndex--;
     if(songIndex< 0){
-      songIndex = songs.length-1;
+      songIndex = 3;
     }
    console.log(songIndex)
    loadSong(songs[songIndex])
@@ -87,7 +89,7 @@ function prevSong(){
 //next song
 function nextSong(){
     songIndex++;
-    if(songIndex > songs.length-1){
+    if(songIndex > 3){
         songIndex= 0
     }
    console.log(songIndex)
@@ -98,6 +100,20 @@ function nextSong(){
 
 //On load - Select First Song
 loadSong(songs[songIndex]);
+
+//Update Progress Bar & Time
+function updateProgressBar(e){
+    if(isPlaying){
+        const{duration, currentTime} = e.srcElement
+        console.log(duration,currentTime)
+        //Update progress bar width
+        const progressPercent = (currentTime / duration) *100;
+        console.log(progressPercent)
+        progress.style.width=`${progressPercent}%`;
+    }
+}
+
 //Event listener
 prevBtn.addEventListener('click',prevSong)
 nextBtn.addEventListener('click',nextSong)
+music.addEventListener('timeupdate',updateProgressBar)
